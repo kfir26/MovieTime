eae07f7f01fec85b39a6017521bd7df36aba7f6f
כפיר פנירי <kpyrpnyry@MacBook-Pro-2.local>
2020-08-23 02:41:08 +0300

    movie Time

--- MovieTime/MovieCore+Extension.swift
+++
@@ -1,9 +0,0 @@
-//
-//  MovieCore+Extension.swift
-//  MovieTime
-//
-//  Created by כפיר פנירי on 06/08/2020.
-//  Copyright © 2020 Kfir. All rights reserved.
-//
-
-import Foundation
--- MovieTime/MovieDataSource.swift
+++
@@ -1,9 +0,0 @@
-//
-//  MovieDataSource.swift
-//  MovieTime
-//
-//  Created by כפיר פנירי on 05/08/2020.
-//  Copyright © 2020 Kfir. All rights reserved.
-//
-
-import Foundation
